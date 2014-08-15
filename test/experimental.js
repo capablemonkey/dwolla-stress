08ee8084ee54cbe4d8c0ff651f1b5bc32fef47cd
Gordon Zheng <technix1@gmail.com>
2014-08-15 09:20:57 -0400

    add experimental test, increasing maxsockets

---
+++ test/experimental.js
@@ -0,0 +1,41 @@
+// experimental: can we do 1000 simultaneous requests?
+// trying to maximize maxSockets, so we can truly send simultaneous requests...
+
+var http = require('http');
+var loadTest = require('./loadTest.js');
+
+var agent = new http.Agent();
+
+agent.maxSockets = 100;
+
+function getGoogle(cb) {
+	http.get({
+	  hostname: 'suggestqueries.google.com',
+	  port: 80,
+	  path: '/complete/search?client=chrome&q=foo',
+	  agent: agent
+	}, function(response) {
+		var str = '';
+
+	  //another chunk of data has been recieved, so append it to `str`
+	  response.on('data', function (chunk) {
+	    str += chunk;
+	  });
+
+	  //the whole response has been recieved, so we just print it out here
+	  response.on('end', function () {
+	    cb(null, str);
+	  });
+	});
+}
+
+getGoogle(console.log);
+
+describe('Experimental', function() {
+	it('Trying to see if inceasing maxSockets is faster', function(done) {
+		loadTest('x-1000', 1000, function(callback) {
+			getGoogle(callback);
+		}, done);
+
+	});
+});
